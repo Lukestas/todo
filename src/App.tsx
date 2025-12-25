1c53168cf1d15e213b3621d90ed2b4680d520e28
Lukestas <julianenriqueluquelopez@gmail.com>
2025-12-24 21:25:03 -0600

    Language of page changed of english to spanish, In App.tsx created the basic structure of the TaskManager To-Do, a h1, form that search and add new tasks, a list with the tasks created, and a delete button in each task, also a button to delete all tasks completed, in the component Task.tsx there is the structure for a task, a li tag with a label tag that has a input checkbox, and a delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useId, useState } from 'react'
 import './App.css'
+import Task from './components/Task'
+import TasksDefault from "./tasksDefault.json"
+import Footer from './components/Footer'
+
+interface TaskType {
+  id: string
+  name: string
+  completed: boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState(TasksDefault)
+  const [search, setSearch] = useState('')
+  const searchId = useId()
+
+  const handleChangeTaskStatus = (id: string) => {
+    setTasks(prevTasks =>
+      prevTasks.map((task) =>
+        task.id === id
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    )
+  }
+
+  const handleSearchTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  const handleAddTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.target as HTMLFormElement)
+    const task = formData.get(searchId)
+    const newTask: TaskType = {
+      id: crypto.randomUUID(),
+      name: task as string,
+      completed: false
+    }
+    setTasks(prev => [...prev, newTask])
+    setSearch('')
+    e.currentTarget.reset()
+  }
+
+  const filteredTasks = tasks.filter(task =>
+    task.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  const handleDeleteTask=(id:string)=>{
+    setTasks(tasks.filter(task=>task.id!==id))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>Lista de tareas</h1>
+      <section>
+        <form onSubmit={handleAddTask}>
+          <input type="text" name={searchId} onChange={handleSearchTask} placeholder='¿Qué tarea esta pendiente?' />
+          <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg></button>
+        </form>
+        <ul>
+          {filteredTasks.map((task, i) => <Task key={i} task={task} onToggle={handleChangeTaskStatus} onDelete={handleDeleteTask}/>)}
+        </ul>
+        <div>
+          <small>{tasks.filter(task => !task.completed).length} tareas pendientes</small>
+          <button >Borrar completas</button>
+        </div>
+      </section>
+      <Footer/>
+    </main>
   )
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p>
+                <a href="https://github.com/Lukestas" target="_blank">Lukestas: </a>
+                This website is under construction.
+            </p>
+        </footer>
+    )
+}
